e22ed0b5c8d6338765a3ab5052bd10a54bab5f12
Muhammad Harish <harish.game02@gmail.com>
2022-07-06 22:26:28 +0700

    Update urls.py

--- semaenergy/urls.py
+++ semaenergy/urls.py
@@ -17,10 +17,7 @@ from django import views
 from django.contrib import admin
 from django.urls import path, include
 
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', include('user.urls')),
+    path('', include('user.urls')),
 ]
